5ebd49fecb33ebe630b2ec71c900c6c16c82d3ce
Erizta Alifa P <eriztaalifad@gmail.com>
2021-06-30 19:20:37 +0700

    Adding Multiple Delivery Points and Calculating the shortest route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import { useEffect, useState, useRef } from 'react'
 import * as tt from '@tomtom-international/web-sdk-maps'
+import * as ttapi from '@tomtom-international/web-sdk-services'
 import './App.css'
 import '@tomtom-international/web-sdk-maps/dist/maps.css'
 
 const App = () => {
   const mapElement = useRef()
   const [map, setMap] = useState({})
-  const [longitude, setLongitude] = useState(110.21778)
-  const [latitude, setLatitude] = useState(-7.47056)
+
+  const [longitude, setLongitude] = useState(110.370529)
+  const [latitude, setLatitude] = useState(-7.797068)
+
+  const convertToPoints = (lngLat) => {
+    return {
+      point: {
+        latitude: lngLat.lng,
+        longitude: lngLat.lng
+      }
+    }
+  }
+
+  const drawRoute=(geoJson)=>{
+    if(map.getLayer('route')){
+      map.removeLayer('route')
+      map.removeSource('route')
+    }
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geoJson
+      },
+      paint: {
+        'line-color':'red',
+        'line-width':6
+      }
+    })
+  }
+
+  const addDeliveryMarker = (lngLat, map) => {
+    const element = document.createElement('div')
+    element.className = 'marker-delivery'
+    new tt.Marker({
+      element: element
+    })
+      .setLngLat(lngLat)
+      .addTo(map)
+  }
 
   useEffect(() => {
+
+    const origin = {
+      lng: longitude,
+      lat: latitude
+    }
+    const destinations = []
+
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
@@ -24,10 +71,10 @@ const App = () => {
     setMap(map)
 
     const addMarker = () => {
-      const popupOffset={
-        bottom: [0,-25]
+      const popupOffset = {
+        bottom: [0, -25]
       }
-      const popup = new tt.Popup({offset:popupOffset}).setHTML('Kamu disini!')
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML('Kamu disini!')
 
       const element = document.createElement('div')
       element.className = 'marker'
@@ -50,6 +97,60 @@ const App = () => {
 
     addMarker()
 
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+      const callParameters = {
+        key: process.env.REACT_APP_TOM_TOM_API_KEY,
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)]
+      }
+
+      return new Promise((resolve, reject) => {
+        ttapi.services
+          .matrixRouting(callParameters)
+          .then((matrixAPIResults) => {
+            const results = matrixAPIResults.matrix[0]
+            const resultsArray = results.map((result, index) => {
+              return {
+                location: locations[index],
+                drivingtime: result.response.routeSummary.travelTimeInSeconds,
+              }
+            })
+            resultsArray.sort((a, b) => {
+              return a.drivingtime - b.drivingtime
+            })
+            const sortedLocations = resultsArray.map((result) => {
+              return result.location
+            })
+            resolve(sortedLocations)
+          })
+      })
+    }
+
+    const recalculateRoutes=()=>{
+      sortDestinations(destinations).then((sorted)=>{
+        sorted.unshift(origin)
+
+        ttapi.services
+        .calculateRoute({
+          key: process.env.REACT_APP_TOM_TOM_API_KEY,
+          locations: sorted,
+        })
+        .then((routeData)=>{
+          const geoJson = routeData.toGeoJson()
+          drawRoute(geoJson,map)
+        })
+      })
+    }
+
+    map.on('click', (e) => {
+      destinations.push(e.lngLat)
+      addDeliveryMarker(e.lngLat, map)
+      recalculateRoutes()
+    })
+
     return () => map.remove()
 
   }, [latitude, longitude])
@@ -60,7 +161,7 @@ const App = () => {
         <div className="App">
           <div ref={mapElement} className="map" />
           <div className="search-bar">
-            <h1>Where to?</h1>
+            <h1>Mau kemana?</h1>
             <input
               type="text"
               id="longitude"
