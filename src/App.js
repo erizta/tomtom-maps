2b2acd3d826dd5e3b20795d2c5f49d41158d8108
Erizta Alifa P <eriztaalifad@gmail.com>
2021-06-30 17:15:04 +0700

    [erxtfd] initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from 'react'
+import './App.css'
+import * as tt from '@tomtom-international/web-sdk-maps'
 
-function App() {
+const App = () => {
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(110.370529)
+  const [latitude, setLatitude] = useState(-7.797068)
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true
+      },
+      center: [longitude, latitude],
+      zoom: 14
+    })
+
+    setMap(map)
+
+    return ()=>map.remove()
+  }, [latitude,longitude])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapElement} className="map" />
+      <div className="search-bar">
+        <h1>Where to?</h1>
+        <input
+          type="text"
+          id="longitude"
+          className="longitude"
+          placeholder="Put the Longitude"
+          onChange={(e) => { setLongitude(e.target.value) }}
+        />
+        <input
+          type="text"
+          id="latitude"
+          className="latitude"
+          placeholder="Put the Latitude"
+          onChange={(e) => { setLatitude(e.target.value) }}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
